ead7dbddbb0c039f9e55b1b111d5367750aab774
fateh beddiaf <kentounmax@gmail.com>
2023-08-30 18:31:53 +0100

    add invite modal

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -8,10 +8,10 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { ServerHeader } from "./server-header";
-import { ServerSearch } from "./server-search";
-import { ServerSection } from "./server-section";
-import { ServerChannel } from "./server-channel";
-import { ServerMember } from "./server-member";
+// import { ServerSearch } from "./server-search";
+// import { ServerSection } from "./server-section";
+// import { ServerChannel } from "./server-channel";
+// import { ServerMember } from "./server-member";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -84,7 +84,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
       <ScrollArea className="flex-1 px-3">
-        <div className="mt-2">
+        {/* <div className="mt-2">
           <ServerSearch
             data={[
               {
@@ -201,7 +201,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
               ))}
             </div>
           </div>
-        )}
+        )} */}
       </ScrollArea>
     </div>
   );
