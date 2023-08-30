079b81a4274158d25ddb3cd7174077636f2d05ce
fateh beddiaf <kentounmax@gmail.com>
2023-08-30 11:49:11 +0100

    add sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,5 @@
+const ServerIdPage = () => {
+  return <div>server page</div>;
+};
+
+export default ServerIdPage;
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,46 @@
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import NavigationAction from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default NavigationSidebar;
