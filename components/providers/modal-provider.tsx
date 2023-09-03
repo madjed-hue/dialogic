77dd815504aa1c4ff7e64ac1cb81828e96e82f8f
fateh beddiaf <kentounmax@gmail.com>
2023-09-03 08:08:03 +0100

    add create chanel modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,7 +6,7 @@ import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
-// import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 // import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 // import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 // import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
@@ -31,8 +31,8 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
-      {/*
       <CreateChannelModal />
+      {/*
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
