6fab98657e389f29750c5e376568e4656d00821e
fateh beddiaf <kentounmax@gmail.com>
2023-09-06 12:06:23 +0100

    add server search ui

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -8,7 +8,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { ServerHeader } from "./server-header";
-// import { ServerSearch } from "./server-search";
+import { ServerSearch } from "./server-search";
 // import { ServerSection } from "./server-section";
 // import { ServerChannel } from "./server-channel";
 // import { ServerMember } from "./server-member";
@@ -84,7 +84,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
       <ScrollArea className="flex-1 px-3">
-        {/* <div className="mt-2">
+        <div className="mt-2">
           <ServerSearch
             data={[
               {
@@ -127,6 +127,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
           />
         </div>
         <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {/* 
         {!!textChannels?.length && (
           <div className="mb-2">
             <ServerSection
