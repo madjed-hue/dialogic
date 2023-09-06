b3190d754778fbb0417e99aac88a2e38b8541f21
fateh beddiaf <kentounmax@gmail.com>
2023-09-06 13:45:32 +0100

    add edit channel api

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,7 +10,7 @@ import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
-// import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 // import { MessageFileModal } from "@/components/modals/message-file-modal";
 // import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
@@ -35,8 +35,8 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
-      {/*
       <EditChannelModal />
+      {/*
       <MessageFileModal />
       <DeleteMessageModal /> */}
     </>
