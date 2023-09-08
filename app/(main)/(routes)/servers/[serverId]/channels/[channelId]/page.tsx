e70e68ab9206aec847b0e5fa10d787db21a3f7a0
fateh beddiaf <kentounmax@gmail.com>
2023-09-08 10:20:58 +0100

    add messages input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,7 +4,7 @@ import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
-// import { ChatInput } from "@/components/chat/chat-input";
+import { ChatInput } from "@/components/chat/chat-input";
 // import { ChatMessages } from "@/components/chat/chat-messages";
 // import { MediaRoom } from "@/components/media-room";
 import { db } from "@/lib/db";
@@ -63,6 +63,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             paramKey="channelId"
             paramValue={channel.id}
           />
+          */}
           <ChatInput
             name={channel.name}
             type="channel"
@@ -71,7 +72,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
               channelId: channel.id,
               serverId: channel.serverId,
             }}
-          /> */}
+          />
         </>
       )}
       {/* {channel.type === ChannelType.AUDIO && (
